809e16ba208bea7bab75c3cb90a924ea787e85d4
joaovirone <jovmamikl@gmail.com>
2026-03-19 22:16:39 -0300

    commit inicial do projeto

---
+++ src/main/kotlin/br/com/investSimulation/simulacao_investimento_app/SimulacaoInvestimentoAppApplication.kt
@@ -0,0 +1,11 @@
+package br.com.investSimulation.simulacao_investimento_app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SimulacaoInvestimentoAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<SimulacaoInvestimentoAppApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/investSimulation/simulacao_investimento_app/SimulacaoInvestimentoAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.investSimulation.simulacao_investimento_app
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SimulacaoInvestimentoAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
