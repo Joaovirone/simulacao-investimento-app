e536be0c720da390d658150f6721cc495817408f
joao <jovmamikl@gmail.com>
2026-03-25 09:07:45 -0300

    feat: implementado url para linkar backend no axios

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333', 
+  baseURL: typeof window !== 'undefined' ? 'http://localhost:3333': 'http://backend:3333' 
 });
