2f72401eb547f4206922405268e8fe79a0552199
joao <jovmamikl@gmail.com>
2026-04-01 08:24:39 -0300

    feat: implementação de token de sessão

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,5 +1,21 @@
+// src/services/api.ts
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: typeof window !== 'undefined' ? 'http://localhost:3333': 'http://backend:3333' 
+  baseURL: typeof window !== 'undefined' ? 'http://localhost:3333' : 'http://backend:3333',
+});
+
+// "Intercepta" todas as requisições antes delas saírem do frontend
+api.interceptors.request.use((config) => {
+  // Verifica se estamos rodando no navegador (client-side)
+  if (typeof window !== 'undefined') {
+    // Busca o token que vamos salvar no LocalStorage no momento do login
+    const token = localStorage.getItem('@InvestSim:token');
+    
+    if (token) {
+      // Se tiver token, anexa ele no cabeçalho de Autorização
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+  return config;
 });
